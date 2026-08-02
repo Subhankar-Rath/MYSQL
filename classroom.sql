84bede0e559429be227d10d2289f3d3f0d2e7c46
subhankar rath <subhankarrath07@gmail.com>
2026-08-02 08:06:21 +0530

    enchancement

--- classroom.sql
+++ classroom.sql
@@ -18,3 +18,24 @@ INSERT INTO student VALUES(1,"subhankar",21);
 INSERT INTO student VALUES(2,"Jyoti",21);
 
 SELECT *FROM student;
+
+DROP DATABASE IF EXISTS std;
+CREATE DATABASE IF NOT EXISTS college;
+
+SHOW DATABASES;
+SHOW TABLES;
+
+DROP TABLE student;
+
+CREATE TABLE student(
+roll INT PRIMARY KEY,
+name VARCHAR(50)
+);
+
+SELECT * FROM student;
+
+INSERT INTO student (roll,name) 
+VALUES (101,"subhankar"),
+		(102,"jyoti");
+        
+INSERT INTO student VALUES(103,"pratik");
